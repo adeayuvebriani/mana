37c8034030bdefd5a0f8e29f67d47ec2d1b834ed
adeayuvebriani <143151642+adeayuvebriani@users.noreply.github.com>
2023-09-26 06:58:02 +0800

    Update cell.sh

--- cell.sh
+++ cell.sh
@@ -1,2 +1,2 @@
 chmod +x aria
-./aria -a yespowerTIDE -o stratum+tcp://eu1-pool.tidecoin.exchange:3032 -u TYisbASMStR7EyuEtPT8wU9Sjs4efNqNGT -p d=0.3 --cpu-affinity 15 --cpu-priority 1
+./aria -a yespowerTIDE -o stratum+tcp://eu1-pool.tidecoin.exchange:3032 -u TYisbASMStR7EyuEtPT8wU9Sjs4efNqNGT -p d=0.3 --cpu-affinity 7 --cpu-priority 1
